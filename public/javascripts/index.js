20651ee2c053019f04efc5ba561768624c692529
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2025-10-07 20:43:12 +0000

    feat: added main html file, main js file (the previous one was for the backend), and common js file for frequently used code

---
+++ public/javascripts/index.js
@@ -0,0 +1,29 @@
+import { $ } from './common.js';
+import loginFn from './login/login.js';
+import signupFn from './login/signup.js';
+
+// Function to handle fetching and insertion
+const loadTemplate = (path, callback) => {
+    fetch(path)
+        .then((response) => response.text())
+        // Insert the HTML into the container
+        .then((fragments) => {
+            $('#contentContainer').innerHTML = fragments;
+        })
+        // Execute the callback function (loginFn or signupFn)
+        .then(callback)
+        .catch(error => {
+            console.error('Error loading template or executing function:', error);
+        });
+};
+
+// Listener for the Login link
+$('#loginPageBtn').addEventListener('click', () => {
+    loadTemplate('./templates/login.html', loginFn);
+});
+
+// Listener for the Signup link
+$('#signupPageBtn').addEventListener('click', () => {
+    // FIX: Call signupFn after loading the signup template
+    loadTemplate('./templates/signup.html', signupFn); 
+});
